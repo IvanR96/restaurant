19df674309d6f7253385f4bb61e0c6f1cabe121a
IvanR96 <irios96@hotmail.com>
2024-01-09 22:41:12 -0800

    js file setup and working with webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert("This is a webpack test!");
+import _ from 'lodash';
+
+function component() {
+    const tabContent = document.getElementById('content');
+
+
+    tabContent.innerHTML = "Welcome to the Bundle test";
+
+    return tabContent;
+}
+
+document.body.appendChild(component());
