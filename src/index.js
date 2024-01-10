7402af48b3757f74491beb8ad2eed4ff9138d5d0
IvanR96 <irios96@hotmail.com>
2024-01-09 23:20:39 -0800

    further work needed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import _ from 'lodash';
+import './style.css';
+import Burger from './burger.jpg';
 
 function component() {
     const tabContent = document.getElementById('content');
 
+    let heading = document.createElement('h1');
 
-    tabContent.innerHTML = "Welcome to the Bundle test";
+    heading.textContent = "Burger Bros";
+
+    tabContent.appendChild(heading);
+
+    let burgerPic =  new Image();
+
+    burgerPic.src = Burger;
+
+    burgerPic.alt = 'burger picture';
+
+
+    tabContent.appendChild(burgerPic);
+
 
     return tabContent;
 }
