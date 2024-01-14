d5f9cb1af4c3586fb8677cffc39ace1b68ec2d14
IvanR96 <irios96@hotmail.com>
2024-01-13 23:47:34 -0800

    tab function imported

--- src/index.js
+++ src/index.js
@@ -1,43 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import Burger from './burger.jpg';
+import { createTabContent1 } from './tab1.js';
 
-function component() {
-    const tabContent = document.getElementById('content');
-    const title = document.createElement('h1');
-    title.textContent = 'Burger Bros';
+createTabContent1();
 
-    tabContent.appendChild(title);
-
-    const tabOne = document.createElement('div');
-
-    tabOne.id = 'tab1';
-    tabOne.classList.add('tab-content');
-
-    const heading = document.createElement('h2');
-    heading.textContent = "Home";
-
-    content.appendChild(heading);
-
-    tabContent.appendChild(tabOne);
-    
-    return tabContent;
-}
-
-
-function createTab1(){
-    const content = document.createElement('div');
-
-    content.id = 'tab1';
-    content.classList.add('tab-content');
-
-    const heading = document.createElement('h2');
-    heading.textContent = "Home";
-
-
-
-}
-//
-
-
-document.body.appendChild(component());
+document.body.appendChild(createTabContent1());
