ce2968f406491e30dadcf66d5d96b5eb14685d5a
IvanR96 <irios96@hotmail.com>
2024-01-13 23:48:02 -0800

    module working

--- src/tab1.js
+++ src/tab1.js
@@ -1,24 +1,28 @@
-function createTabContent1() {
-    const tabContent = document.createElement('div');
-    tabContent.id = 'tab1';
-    tabContent.classList.add('tab-content');
+export function createTabContent1() {
+    const tabOne = document.createElement('div');
+    tabOne.id = 'tab1';
+    tabOne.classList.add('tab-content');
+
+    let title = document.createElement('h1');
+    title.textContent = "Burger Bros";
+
+    tabOne.appendChild(title);
   
     // Create a clickable link (anchor element)
     const headingLink = document.createElement('a');
     headingLink.href = 'index.html'; // Set the URL of your home page
-    headingLink.target = '_blank'; // Open link in a new tab/window (optional)
   
     const heading = document.createElement('h2');
-    heading.textContent = 'Content for Tab 1';
+    heading.textContent = 'Home';
   
     // Append the heading to the link
     headingLink.appendChild(heading);
   
-    const paragraph = document.createElement('p');
-    paragraph.textContent = 'This is the content of Tab 1.';
+    //const paragraph = document.createElement('p');
+    //paragraph.textContent = 'This is the content of Tab 1.';
   
-    tabContent.appendChild(headingLink); // Append the link instead of the heading directly
-    tabContent.appendChild(paragraph);
+    tabOne.appendChild(headingLink); // Append the link instead of the heading directly
   
-    return tabContent;
+  
+    return tabOne;
   }
