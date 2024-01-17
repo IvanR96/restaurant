58c3dce6010ed1b34730fe60866837b816dc8c77
IvanR96 <irios96@hotmail.com>
2024-01-16 23:28:09 -0800

    needs updating

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 import { createTabContent1 } from './tab1.js';
+import { createTabContent2 } from './tab2.js';
 
 createTabContent1();
+createTabContent2();
 
 document.body.appendChild(createTabContent1());
+document.body.appendChild(createTabContent2());
+
