7bbe339565617423db5614c21d30ccdb36b8acba
IvanR96 <irios96@hotmail.com>
2024-01-17 23:09:46 -0800

    home tab created

---
+++ src/homeMod.js
@@ -0,0 +1,25 @@
+function homeMod(){
+  const homeDiv = document.createElement('div');
+
+  // creation of content 
+
+  const headline = document.createElement('h2');
+  headline.textContent = 'Burger Bros';
+
+  const burgerPic = document.createElement('img');
+  burgerPic.src = './burger.jpg';
+  burgerPic.alt = 'picture of a burger';
+  burgerPic.id = 'burger';
+
+  const paragraph = document.createElement('p');
+  paragraph.textContent = 'For over 10 years we have worked hard at Burger Bros to create an unforgettable burger experience!'
+
+
+  homeDiv.appendChild(headline);
+  homeDiv.appendChild(burgerPic);
+  homeDiv.appendChild(paragraph);
+
+  return homeDiv;
+}
+
+export default homeMod;
