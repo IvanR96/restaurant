65484ca1a9d6960aee88ac5e4b0275afb98444d3
IvanR96 <irios96@hotmail.com>
2024-01-19 22:15:00 -0800

    index file changed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import _ from 'lodash';
 import './style.css';
-import { createTabContent1 } from './homeMod.js';
-import { createTabContent2 } from './tab2.js';
-
-createTabContent1();
-createTabContent2();
-
-document.body.appendChild(createTabContent1());
-document.body.appendChild(createTabContent2());
-
+import contactMod from './contactMod';
+import homeMod from './homeMod';
