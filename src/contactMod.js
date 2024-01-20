7d1138fc8732dd7ec58114cdd7d0d6a52b96b083
IvanR96 <irios96@hotmail.com>
2024-01-19 21:49:54 -0800

    contact module finished

--- src/contactMod.js
+++ src/contactMod.js
@@ -1,5 +1,16 @@
 function contactMod(){
-    
+    const contactDiv = document.createElement('div');
+
+    const contactHeader = document.createElement('h2');
+    contactHeader.textContent = 'Contact Us';
+
+    const contactInfo = document.createElement('p');
+    contactInfo.textContent = 'Please feel free to contact us!\nWe are located at 223 Tech Ave, Cupertino,Ca\nPhone number is 123-456-7890';
+
+    contactDiv.appendChild(contactHeader);
+    contactDiv.appendChild(contactInfo);
+
+    return contactDiv;
 }
 
 
