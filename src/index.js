0faddec78081423bbd24555f534f490f0f5e4cdb
IvanR96 <irios96@hotmail.com>
2024-01-21 23:29:11 -0800

    index file js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,7 @@
 import _ from 'lodash';
 import './style.css';
-import contactMod from './contactMod';
-import homeMod from './homeMod';
-import initPageLoad from './initPageLoad';
-import './burger.jpg';
+import initPageLoad from "./initPageLoad";
 
 document.addEventListener('DOMContentLoaded', () => {
     initPageLoad();
-
-    const homeLink = document.getElementById('homeLink');
-    const menuLink = document.getElementById('menuLink');
-    const contactLink = document.getElementById('contactLink');
-    const contentDiv = document.getElementById('content');
-
-    homeLink.addEventListener('click', (event) => {
-        event.preventDefault();
-        clearContent();
-        contentDiv.appendChild(homeMod()); // Use the home module here
-    });
-
-    menuLink.addEventListener('click', (event) => {
-        event.preventDefault();
-        clearContent();
-        contentDiv.appendChild(menuModule());
-    });
-
-    contactLink.addEventListener('click', (event) => {
-        event.preventDefault();
-        clearContent();
-        contentDiv.appendChild(contactMod());
-    });
-
-    function clearContent() {
-        while (contentDiv.firstChild) {
-            contentDiv.removeChild(contentDiv.firstChild);
-        }
-    }
 });
